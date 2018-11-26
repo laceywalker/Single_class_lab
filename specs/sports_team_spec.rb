707ee361a3c9e1c1b07b2bdeb35ca70ed5f11abe
Michael Irwin <mike.irwin1@gmail.com>
2018-11-26 16:13:23 +0000

    sports team getter methods

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -10,6 +10,15 @@ def test_team_name
   assert_equal("Eyebrows", new_team.get_name)
 end
 
+def test_team_players
+  new_team = Team.new("Eyebrows", ['Tom', 'Bob','Sarah'], "Coach")
+  assert_equal(['Tom', 'Bob','Sarah'], new_team.get_players)
+end
+
+def test_team_coach
+  new_team = Team.new("Eyebrows", ['Tom', 'Bob','Sarah'], "Coach")
+  assert_equal("Coach", new_team.get_coach)
+end
 
 
 
--- sports_team.rb
+++ sports_team.rb
@@ -12,7 +12,13 @@ def get_name
   return @name
 end
 
+def get_players
+  return @players
+end
 
+def get_coach
+  return @coach
+end
 
 
 
