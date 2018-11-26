c1345b141957502f543dfdd3cb8df643d99a5c45
Michael Irwin <mike.irwin1@gmail.com>
2018-11-26 15:50:03 +0000

    I can talk completed

--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -6,19 +6,23 @@ class TestStudent < MiniTest::Test
 
   def test_student_name
     # Arrange
-    student = Student.new('Billy', 'E27')
+    student = Student.new('Billy', 'E27','I can talk!')
     # Act
-
     # Assert
     assert_equal('Billy', student.name)
   end
 
   def test_student_cohort
     # Arrange
-    student = Student.new('Billy','E27')
+    student = Student.new('Billy','E27', 'I can talk!')
     # Act
-
     # Assert
     assert_equal('E27', student.cohort)
+  end
+
+  def test_student_talking
+    student = Student.new('Alice', 'E28', 'I can talk!')
+    # phrase = student_talking()
+    assert_equal('I can talk!', student.phrase)
   end
 end
--- student.rb
+++ student.rb
@@ -1,8 +1,14 @@
 class Student
 
-  attr_reader :name,:cohort
-  def initialize(student_name, cohort)
+  attr_reader :name,:cohort, :phrase
+  attr_writer :name, :cohort, :phrase
+  def initialize(student_name, cohort, phrase)
     @name = student_name
     @cohort = cohort
+    @phrase = phrase
   end
+#
+#   def student_talking
+#     return 'I can talk!'
+#   end
 end
